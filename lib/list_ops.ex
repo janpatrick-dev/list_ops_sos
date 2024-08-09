431bdf6a39b1f32b6fc08999877e328c2c6a4ba2
Matty O <matt6ong6@gmail.com>
2024-08-09 12:38:12 +0800

    Initial commit

---
+++ lib/list_ops.ex
@@ -0,0 +1,18 @@
+defmodule ListOps do
+  @moduledoc """
+  Documentation for `ListOps`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ListOps.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/list_ops_test.exs
@@ -0,0 +1,8 @@
+defmodule ListOpsTest do
+  use ExUnit.Case
+  doctest ListOps
+
+  test "greets the world" do
+    assert ListOps.hello() == :world
+  end
+end
