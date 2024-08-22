1298e6e2c0942e01f10f39de0fe51f49a1e79aab
Matty O <matt6ong6@gmail.com>
2024-08-22 14:06:48 +0800

    Adds list ops functions  and tests

--- lib/list_ops.ex
+++ lib/list_ops.ex
@@ -1,18 +1,31 @@
 defmodule ListOps do
-  @moduledoc """
-  Documentation for `ListOps`.
-  """
+  # Please don't use any external modules (especially List or Enum) in your
+  # implementation. The point of this exercise is to create these basic
+  # functions yourself. You may use basic Kernel functions (like `Kernel.+/2`
+  # for adding numbers), but please do not use Kernel functions for Lists like
+  # `++`, `--`, `hd`, `tl`, `in`, and `length`.
 
-  @doc """
-  Hello world.
+  def count(list) do
+  end
 
-  ## Examples
+  def reverse(list) do
+  end
 
-      iex> ListOps.hello()
-      :world
+  def map(list, fun) do
+  end
 
-  """
-  def hello do
-    :world
+  def filter(list, fun) do
+  end
+
+  def foldlist(list, acc, fun) do
+  end
+
+  def foldr(list, acc, fun) do
+  end
+
+  def append(list_a, list_b) do
+  end
+
+  def concat(ll) do
   end
 end
---
+++ lib/recursion.ex
@@ -0,0 +1,6 @@
+defmodule RecursionExample do
+  def print_x_times(string, count) do
+
+    print_x_times(string, count)
+  end
+end
